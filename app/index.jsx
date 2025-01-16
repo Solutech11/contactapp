4f8b3ee7d04bae0f84e73ec84f54ac1c19181fa3
Solutech11 <soluwizy@gmail.com>
2025-01-16 12:50:46 +0100

    update db

--- app/index.jsx
+++ app/index.jsx
@@ -3,9 +3,71 @@ import { Text, View, StyleSheet, TextInput, Button, Pressable, Image, Keyboard, 
 import AuthLayout from "./Layouts/AuthLayout";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
+import * as SQLite from 'expo-sqlite';
+import { useEffect, useState } from "react";
 
 const logo = require("./../assets/images/splash-img.png");
 export default function HomeScreen() {
+
+  const [db, setdb] = useState(null)
+
+  const [Contacts, setContacts] = useState([])
+
+  
+
+  async function initializeDb(db) {
+    try {
+      await db.execAsync(`
+        PRAGMA journal_mode = WAL;
+        CREATE TABLE IF NOT EXISTS contacts (
+          id INTEGER PRIMARY KEY NOT NULL, 
+          name TEXT NOT NULL, 
+          phone TEXT NOT NULL
+        );
+        
+      `);
+
+        console.log('Table initialized successfuly');
+        
+    } catch (error) {
+      console.error("error initializing db", error)
+    }
+  }
+
+  async function FetchContacts(db) {
+    try {
+      let data= await db.getAllAsync('SELECT * FROM contacts');
+      setContacts(data)
+    } catch (error) {
+      console.error("error fetching db", error)
+    }
+  }
+
+  async function AddContact(db) {
+    try {
+      let name=`John Dolphin ${Math.random().toFixed(2)}`;
+      let phone= '123-456,7890'
+
+      const result = await db.runAsync('INSERT INTO contacts (name, phone) VALUES (?, ?)', name, phone);
+      await FetchContacts(db)
+      
+    } catch (error) {
+      
+      console.error("error fetching db", error)
+    }
+  }
+
+  useEffect(() => {
+    (async()=>{
+      const newdb = await SQLite.openDatabaseAsync('contacts');
+      setdb(newdb)
+      await initializeDb(newdb)
+      await FetchContacts(newdb)
+      AddContact(db)
+
+    })()
+  }, [])
+  
   return (
     <SafeAreaView style={{ flex: 1 }}>
           <StatusBar style="dark" />
@@ -23,15 +85,25 @@ export default function HomeScreen() {
               <View style={{ flex: 1 , paddingVertical:30, paddingHorizontal:20}}>
                 <ScrollView>
 
-                  {/* items */}
-                  <TouchableOpacity style={styles.ItemView}>
-                    <Image source={require('../assets/images/splash-img.png')} style={{width:40, height:40}} />
+                  {
+                    Contacts.length>0?(
+                      Contacts.map((i,key)=>(
+                        <TouchableOpacity style={styles.ItemView}>
+                          <Image source={require('../assets/images/splash-img.png')} style={{width:40, height:40}} />
 
-                    <View>
-                      <Text style={styles.ItemText}>Swaziburg Duoni </Text>
-                      <Text style={[styles.ItemText,{fontSize:13}]}>08133092341</Text>
-                    </View>
-                  </TouchableOpacity>
+                          <View>
+                            <Text style={styles.ItemText}>{i.name} </Text>
+                            <Text style={[styles.ItemText,{fontSize:13}]}>{i.phone}</Text>
+                          </View>
+                        </TouchableOpacity>
+                        ))
+                      
+                    ):(
+                      <Text>No contacts found</Text>
+                    )
+                  }
+                  {/* items */}
+                  
 
 
                 </ScrollView>
@@ -78,7 +150,8 @@ const styles = StyleSheet.create({
     flexDirection:'row',
     padding:10,
     borderRadius:10,
-    gap:10
+    gap:10,
+    marginBottom:10
   },
   ItemText:{
     color:'white',
